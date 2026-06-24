bf5e03da86a4c5f0e31cf67eea7f29371054ac6f
joaohenriqueribeiro81-star <joaohenriqueribeiro81@gmail.com>
2026-06-23 21:40:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+function criarEstrela(){
+
+    const estrela = document.createElement("img");
+
+    estrela.src = "estrelaa.png";
+
+    estrela.classList.add("decorativa");
+
+    estrela.style.left = Math.random() * 95 + "%";
+
+    estrela.style.top = Math.random() * 95 + "%";
+
+    document.body.appendChild(estrela);
+
+    estrela.addEventListener("animationend", ()=>{
+
+        estrela.remove();
+
+    });
+
+}
+
+criarEstrela();
+
+
