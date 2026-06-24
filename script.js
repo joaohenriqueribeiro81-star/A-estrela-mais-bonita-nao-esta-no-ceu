5eadb16bd8dca5c6896487e1d5a9b2198b73ea68
joaohenriqueribeiro81-star <joaohenriqueribeiro81@gmail.com>
2026-06-23 22:13:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
+console.log("SCRIPT OK");
 
 function criarEstrela(){
+    console.log("criando estrela");
 
     const estrela = document.createElement("img");
-
     estrela.src = "estrelaa.png";
-
     estrela.classList.add("decorativa");
 
     estrela.style.left = Math.random() * 95 + "%";
-
     estrela.style.top = Math.random() * 95 + "%";
 
     document.body.appendChild(estrela);
 
-    estrela.addEventListener("animationend", ()=>{
-
+    estrela.addEventListener("animationend", () => {
         estrela.remove();
-
     });
-
 }
 
-criarEstrela();
+function iniciarCeu(){
+    console.log("ceu iniciado");
 
+    setInterval(criarEstrela, 400);
+}
 
+iniciarCeu();
